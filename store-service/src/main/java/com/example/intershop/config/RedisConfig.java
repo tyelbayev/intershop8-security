1670f6fc35af9f53c937cfba910d7a21552b4593
turar <tyelbayev@gmail.com>
2025-05-24 21:30:30 +0500

    final

--- store-service/src/main/java/com/example/intershop/config/RedisConfig.java
+++ store-service/src/main/java/com/example/intershop/config/RedisConfig.java
@@ -4,24 +4,22 @@ import com.example.intershop.model.Item;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.*;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public ReactiveRedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-    @Bean
-    public ReactiveRedisTemplate<String, Item> redisTemplate(ReactiveRedisConnectionFactory factory) {
+    public ReactiveRedisTemplate<String, Item> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        Jackson2JsonRedisSerializer<Item> serializer = new Jackson2JsonRedisSerializer<>(Item.class);
         RedisSerializationContext<String, Item> context = RedisSerializationContext
-                .<String, Item>newSerializationContext(new StringRedisSerializer())
-                .value(new Jackson2JsonRedisSerializer<>(Item.class))
+                .<String, Item>newSerializationContext(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .value(serializer)
                 .build();
+
         return new ReactiveRedisTemplate<>(factory, context);
     }
 }
